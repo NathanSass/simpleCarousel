6a427a72848e51d8b01b04603045aa0fe46b329c
Nathan Sass <nsass711@gmail.com>
2016-10-20 10:59:11 -0700

    basic structure

---
+++ app.js
@@ -0,0 +1,21 @@
+var domReady = function(callback) {
+    document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
+};
+
+domReady(function() {
+    // Your code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
